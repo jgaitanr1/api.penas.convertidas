952590b37569e4d079bc4ee4fcc2a91a336ec9db
jgaitanr <jgaitanr@outlook.com>
2023-11-03 15:43:08 -0500

    Prueba

--- src/main/java/com/penasconvertidas/api/models/User.java
+++ src/main/java/com/penasconvertidas/api/models/User.java
@@ -37,7 +37,6 @@ public class User {
 	private String nroTelefono;
 	private String institucion;
 	private boolean estado;
-	
 
 	@ManyToMany(fetch = FetchType.LAZY)
 	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
